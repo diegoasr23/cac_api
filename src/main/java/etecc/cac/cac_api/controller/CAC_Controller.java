54a53849ca7f63d978597171fb09fd439141ad51
diegoasr23 <diegoasr2023@outlook.es>
2024-07-01 13:59:22 -0400

    Skipped the mysql healthckeck

--- src/main/java/etecc/cac/cac_api/controller/CAC_Controller.java
+++ src/main/java/etecc/cac/cac_api/controller/CAC_Controller.java
@@ -10,8 +10,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 @RestController
 @RequestMapping("/cac")
